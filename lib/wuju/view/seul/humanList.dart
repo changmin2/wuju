cbd0d6026f35041cc95a52edae1f6ba866efe74f
rsy0921 <rsy0921@naver.com>
2023-11-20 22:36:36 +0900

    나를 만나고 싶은 사람 디자인 변경

--- lib/wuju/view/seul/humanList.dart
+++ lib/wuju/view/seul/humanList.dart
@@ -21,11 +21,16 @@ class _HumanListState extends State<HumanList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0.0,
-        title:
-            Text("나를 만나고 싶은 외계인", style: TextStyle(color: Color(0xff12887A))),
+        title: Text(
+          "나를 만나고 싶은 외계인",
+          style: TextStyle(
+            color: Color(0xff12887A),
+          ),
+        ),
         centerTitle: true,
         leading: IconButton(
           icon: Icon(
@@ -39,99 +44,104 @@ class _HumanListState extends State<HumanList> {
         ),
       ),
       body: ListView(
-          children: indexList
-              .map((e) => Container(
-                  height: 122,
-                  padding: EdgeInsets.fromLTRB(15, 10, 15, 0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black26, width: 1),
-                        color: Colors.white),
-                    child: Row(children: [
-                      SizedBox(
-                        width: 110,
-                        height: 110,
-                        child: Image.asset(
-                          "asset/img/home_profile.png",
-                          fit: BoxFit.fill,
-                        ),
+        children: indexList
+            .map(
+              (e) => Container(
+                height: 120,
+                padding: EdgeInsets.fromLTRB(15, 10, 15, 0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Color(0xffE5E5E5),
+                      width: 1,
+                    ),
+                  ),
+                  child: Row(
+                    children: [
+                      Image.asset(
+                        "asset/img/home_profile.png",
+                        fit: BoxFit.fill,
+                        width: 120,
+                        height: 120,
                       ),
-                      Padding(
+                      Container(
                         padding: EdgeInsets.all(10),
-                        child: SizedBox(
-                            width: 198,
-                            height: 110,
-                            child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.start,
-                                      children: [
-                                        Text(nameList[e], //이름
-                                            style: TextStyle(
-                                                fontSize: 18,
-                                                fontWeight: FontWeight.w700)),
-                                        sexList[e] == '여'
-                                            ? Icon(Icons.female,
-                                                color: Colors.red)
-                                            : Icon(Icons.male,
-                                                color: Colors.blue)
-                                      ]),
-                                  SizedBox(
-                                    width: 190,
+                        width: MediaQuery.of(context).size.width - 152,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                Text(
+                                  nameList[e], //이름
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                                sexList[e] == '여'
+                                    ? Icon(Icons.female, color: Colors.red)
+                                    : Icon(Icons.male, color: Colors.blue)
+                              ],
+                            ),
+                            Text(
+                              comentList[e], //하고싶은말
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Color(0xff9a9a9a),
+                              ),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                SizedBox(
+                                  width: 60,
+                                  height: 30,
+                                  child: FilledButton(
+                                    onPressed: () {
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (context) => DetailAlien2(),
+                                        ),
+                                      );
+                                    },
+                                    style: ButtonStyle(
+                                      backgroundColor:
+                                          MaterialStateProperty.all<Color>(
+                                        Color(0xffD3F2FF),
+                                      ),
+                                      shape: MaterialStateProperty.all<
+                                          RoundedRectangleBorder>(
+                                        RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(10),
+                                        ),
+                                      ),
+                                    ),
                                     child: Text(
-                                      comentList[e], //하고싶은말
-                                      overflow: TextOverflow.ellipsis,
+                                      '더보기',
                                       style: TextStyle(
-                                          fontSize: 18,
-                                          color: Color(0xff9a9a9a)),
+                                        fontSize: 10,
+                                        color: Color(0xff194BFC),
+                                      ),
                                     ),
                                   ),
-                                  SizedBox(
-                                    width: 20,
-                                    height: 10,
-                                  ),
-                                  Row(
-                                      mainAxisAlignment: MainAxisAlignment.end,
-                                      children: [
-                                        SizedBox(
-                                          width: 80,
-                                          height: 25,
-                                          child: FilledButton(
-                                            onPressed: () {
-                                              Navigator.push(
-                                                  context,
-                                                  MaterialPageRoute(
-                                                      builder: (context) =>
-                                                          DetailAlien2()));
-                                            },
-                                            child: Text('더보기',
-                                                style: TextStyle(
-                                                    fontSize: 15,
-                                                    color: Color(0xff276ac9))),
-                                            style: ButtonStyle(
-                                                backgroundColor:
-                                                    MaterialStateProperty.all<
-                                                            Color>(
-                                                        Color(0xffd6ecff)),
-                                                shape: MaterialStateProperty.all<
-                                                        RoundedRectangleBorder>(
-                                                    RoundedRectangleBorder(
-                                                        borderRadius:
-                                                            BorderRadius
-                                                                .circular(
-                                                                    10)))),
-                                          ),
-                                        ),
-                                      ])
-                                ])),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
                       )
-                    ]),
-                  )))
-              .toList()),
+                    ],
+                  ),
+                ),
+              ),
+            )
+            .toList(),
+      ),
     );
   }
 }
